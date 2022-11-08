dc5044ca9ae2e1834a0b36f6af712e63810cfa8f
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-08 12:44:08 +0600

    added login & signup

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,49 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaLongArrowAltRight } from 'react-icons/fa';
 const Header = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link>Item 1</Link></li>
+    <div className='bg-[#002333]'>
+      <div className="navbar w-full md:w-11/12 mx-auto">
+        <div className="navbar-start text-white">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle hover:bg-gray-100/10 lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-[#002333]">
+              <li><Link to='/'>Home</Link></li>
+              <li tabIndex={0}>
+                <Link className="justify-between">
+                  Services
+                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                </Link>
+                <ul className="p-2 bg-[#002333]">
+                  <li><Link>Submenu 1</Link></li>
+                </ul>
+              </li>
+              <li><Link to='/blog'>Blog</Link></li>
+            </ul>
+          </div>
+          <Link to='/' className="text-xl md:text-2xl font-semibold text-white">Travel<span className='font-bold text-[#2bf29c]'>aro</span></Link>
+        </div>
+        <div className="navbar-center hidden lg:flex text-white">
+          <ul className="menu menu-horizontal p-0">
+            <li><Link to='/'>Home</Link></li>
             <li tabIndex={0}>
-              <Link className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+              <Link>
+                Services
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
               </Link>
-              <ul className="p-2">
+              <ul className="p-2 bg-[#002333]">
                 <li><Link>Submenu 1</Link></li>
                 <li><Link>Submenu 2</Link></li>
               </ul>
             </li>
-            <li><Link>Item 3</Link></li>
+            <li><Link to='/blog'>Blog</Link></li>
           </ul>
         </div>
-        <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal p-0">
-          <li><Link>Item 1</Link></li>
-          <li tabIndex={0}>
-            <Link>
-              Parent
-              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-            </Link>
-            <ul className="p-2">
-              <li><Link>Submenu 1</Link></li>
-              <li><Link>Submenu 2</Link></li>
-            </ul>
-          </li>
-          <li><Link>Item 3</Link></li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <Link className="btn">Get started</Link>
+        <div className="navbar-end">
+          <Link to='/signup' className="text-center bg-[#2bf29c] hover:bg-[#19e98f] py-2 px-3 rounded-md">Get Started<FaLongArrowAltRight className='inline-block ml-2' /> </Link>
+        </div>
       </div>
     </div>
   );
--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Error from "../../Pages/Error/Error";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ export const router = createBrowserRouter([
       {
         path: '/home',
         element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/signup',
+        element: <SignUp></SignUp>
       }
     ]
   },
