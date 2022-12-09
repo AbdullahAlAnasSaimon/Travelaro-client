6fc35d1f3578e272c3b10e30218b1377cdd057ce
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-09 16:50:05 +0600

    added some styles

--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -5,7 +5,7 @@ import { PhotoProvider, PhotoView } from 'react-photo-view';
 const Service = ({ service }) => {
   const { _id, name, price, rating, photo, details } = service;
   return (
-    <div className="w-auto border-2 rounded-md">
+    <div className="w-auto border-2 rounded-md shadow-sm hover:shadow-xl duration-300">
       <div className='h-[305px] p-3'>
         <PhotoProvider>
           <PhotoView src={photo}>
@@ -15,11 +15,11 @@ const Service = ({ service }) => {
       </div>
       <div className="card-body relative">
         <p>{rating}</p>
-        <h2 className="card-title absolute top-4">{name}</h2>
-        <p className='text-[15px] absolute top-16'>{details.slice(0, 100) + '...'}</p>
-        <div className="flex justify-between mt-5 absolute bottom-0">
+        <h2 className="card-title">{name}</h2>
+        <p className='text-[15px] h-[65px]'>{details.slice(0, 100) + '...'}</p>
+        <div className="flex justify-between mt-5">
           <h2 className='text-4xl font-bold'>${price}</h2>
-          <Link className='' to={`/services/${_id}`}><button className="bg-emerald-300 hover:bg-emerald-400 py-2 px-4 text-lg rounded-md">View Details</button></Link>
+          <Link className='' to={`/services/${_id}`}><button className="bg-emerald-300 hover:bg-emerald-400 duration-300 py-2 px-4 text-lg rounded-md">View Details</button></Link>
         </div>
       </div>
     </div>
