fefc985fd17cca24742c22c4d660d257ca2cf1a7
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-27 23:51:36 +0600

    modified review card

--- src/Pages/ServiceDetails/ShowReviews/ShowReviews.js
+++ src/Pages/ServiceDetails/ShowReviews/ShowReviews.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaStar} from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 
 const ShowReviews = ({ review }) => {
   const { photo, description, insertTime, userName } = review;
@@ -7,25 +7,27 @@ const ShowReviews = ({ review }) => {
   // console.log(insertTime.split(''))
   return (
     <div className='border-2 p-3 my-4 w-10/12 mx-auto rounded-md'>
-      <div className='flex items-center'>
+      <div className='flex justify-between'>
+        <div className='flex items-center'>
+          <div>
+            <label tabIndex={0} className="btn-sm btn-ghost btn-circle avatar">
+              <img className='w-10 rounded-full' src={photo} alt="" />
+            </label>
+          </div>
+          <div className='ml-2'>
+            <h4 className='text-sm font-semibold'>{userName}</h4>
+            <p className='text-[11px]'>{newTime.slice(0, 24)}</p>
+          </div>
+        </div>
         <div>
-        <label tabIndex={0} className="btn-sm btn-ghost btn-circle avatar">
-          <img className='w-10 rounded-full' src={photo} alt="" />
-        </label>
-        </div>
-        <div className='ml-2'>
-          <h4 className='text-sm font-semibold'>{userName}</h4>
-          <p className='text-[11px]'>{newTime.slice(0, 24)}</p>
+          <FaStar className='inline-block text-[#3ccf8f]' />
+          <FaStar className='inline-block text-[#3ccf8f]' />
+          <FaStar className='inline-block text-[#3ccf8f]' />
+          <FaStar className='inline-block text-[#3ccf8f]' />
+          <FaStar className='inline-block text-[#3ccf8f]' />
         </div>
       </div>
-      <div>
-        <FaStar className='inline-block text-[#3ccf8f]'/>
-        <FaStar className='inline-block text-[#3ccf8f]'/>
-        <FaStar className='inline-block text-[#3ccf8f]'/>
-        <FaStar className='inline-block text-[#3ccf8f]'/>
-        <FaStar className='inline-block text-[#3ccf8f]'/>
-      </div>
-      <p className='mt-3 mb-2'>{description}</p>
+      <p className='mt-2'>{description}</p>
     </div>
   );
 };
